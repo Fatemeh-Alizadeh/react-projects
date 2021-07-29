5b75eccd204cb5bb4b8bf08897923f038de0e760
Fatemeh-Alizadeh <82482430+Fatemeh-Alizadeh@users.noreply.github.com>
2021-07-29 11:18:42 +1000

    complete

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,7 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
   const removeTour = (id) => {
@@ -26,11 +26,10 @@ function App() {
       console.log(error)
     }
    
-    console.log(tours)
   }
   useEffect(() => {
     fetchTours();
-  }, [])
+  }, []);
 
   if (loading) {
     return <main>
